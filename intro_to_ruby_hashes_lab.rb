ef5de848d378543182fbcd117d4185d9ad46abbe
Toni-Ty <antonique2479@yahoo.com>
2019-02-11 03:34:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,9 +62,7 @@ end
 		:names=>
 		{
 			:reading_railroad=>
-			{
-				"mortgage_value"=>"$100"
-			},
+			{"mortgage_value"=>"$100"},
 			:pennsylvania_railroad=>
 			{
 				"mortgage_value"=>"$200"
